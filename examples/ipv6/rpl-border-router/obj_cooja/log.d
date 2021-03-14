c86a1136332e256b895aa2f9a3be15eef185f493
User <liviobiondo@gmail.com>
2021-03-14 17:13:25 +0100

    added logs for mqqt demo, simulation is working with border router

---
+++ examples/ipv6/rpl-border-router/obj_cooja/log.d
@@ -0,0 +1,40 @@
+obj_cooja/log.o: ../../../platform/cooja/sys/log.c \
+ ../../../core/./sys/log.h ../../../core/./net/linkaddr.h \
+ ../../../platform/cooja/./contiki-conf.h project-conf.h \
+ ../../../core/./sys/log-conf.h ../../../core/./net/ipv6/uip.h \
+ ../../../core/./net/ipv6/uipopt.h ../../../core/./contiki.h \
+ ../../../core/./contiki-version.h ../../../core/./contiki-default-conf.h \
+ ../../../core/./sys/process.h ../../../core/./sys/pt.h \
+ ../../../core/./sys/lc.h ../../../core/./sys/lc-switch.h \
+ ../../../core/./sys/cc.h ../../../core/./sys/cc-gcc.h \
+ ../../../core/./sys/autostart.h ../../../core/./sys/process.h \
+ ../../../core/./sys/timer.h ../../../core/./sys/clock.h \
+ ../../../core/./sys/ctimer.h ../../../core/./sys/etimer.h \
+ ../../../core/./sys/timer.h ../../../core/./sys/etimer.h \
+ ../../../core/./sys/rtimer.h ../../../platform/cooja/./rtimer-arch.h \
+ ../../../platform/cooja/./contiki-conf.h ../../../core/./sys/pt.h \
+ ../../../core/./sys/procinit.h ../../../core/./sys/loader.h \
+ ../../../core/./sys/clock.h ../../../core/./sys/energest.h \
+ ../../../core/./sys/rtimer.h ../../../core/./net/ipv6/uipbuf.h \
+ ../../../core/./net/ipv6/tcpip.h \
+ ../../../platform/cooja/./lib/simEnvChange.h
+../../../platform/cooja/sys/log.c :
+ ../../../core/./sys/log.h ../../../core/./net/linkaddr.h :
+ ../../../platform/cooja/./contiki-conf.h project-conf.h :
+ ../../../core/./sys/log-conf.h ../../../core/./net/ipv6/uip.h :
+ ../../../core/./net/ipv6/uipopt.h ../../../core/./contiki.h :
+ ../../../core/./contiki-version.h ../../../core/./contiki-default-conf.h :
+ ../../../core/./sys/process.h ../../../core/./sys/pt.h :
+ ../../../core/./sys/lc.h ../../../core/./sys/lc-switch.h :
+ ../../../core/./sys/cc.h ../../../core/./sys/cc-gcc.h :
+ ../../../core/./sys/autostart.h ../../../core/./sys/process.h :
+ ../../../core/./sys/timer.h ../../../core/./sys/clock.h :
+ ../../../core/./sys/ctimer.h ../../../core/./sys/etimer.h :
+ ../../../core/./sys/timer.h ../../../core/./sys/etimer.h :
+ ../../../core/./sys/rtimer.h ../../../platform/cooja/./rtimer-arch.h :
+ ../../../platform/cooja/./contiki-conf.h ../../../core/./sys/pt.h :
+ ../../../core/./sys/procinit.h ../../../core/./sys/loader.h :
+ ../../../core/./sys/clock.h ../../../core/./sys/energest.h :
+ ../../../core/./sys/rtimer.h ../../../core/./net/ipv6/uipbuf.h :
+ ../../../core/./net/ipv6/tcpip.h :
+ ../../../platform/cooja/./lib/simEnvChange.h :
