aa2d97cdad95a8420051d6696ce8a6055c71f8a1
liviobi <liviobiondo@gmail.com>
2021-03-17 23:46:55 +0100

    added rime structures to mqtt

--- examples/0node/mqtt-demo.c
+++ examples/0node/mqtt-demo.c
@@ -1,46 +1,3 @@
-/*
- * Copyright (c) 2014, Texas Instruments Incorporated - http://www.ti.com/
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-/*---------------------------------------------------------------------------*/
-/** \addtogroup cc2538-examples
- * @{
- *
- * \defgroup cc2538-mqtt-demo CC2538 MQTT Demo Project
- *
- * Demonstrates MQTT functionality. Works with IBM Quickstart as well as
- * mosquitto.
- * @{
- *
- * \file
- * An MQTT example for the cc2538-based platforms
- */
-/*---------------------------------------------------------------------------*/
 #include "contiki-conf.h"
 #include "rpl/rpl-private.h"
 #include "mqtt.h"
@@ -53,20 +10,67 @@
 #include "lib/sensors.h"
 #include "dev/button-sensor.h"
 #include "dev/leds.h"
-
 #include "contiki.h"
 #include "lib/list.h"
 #include "lib/memb.h"
 #include "lib/random.h"
 #include "net/rime/rime.h"
-
 #include <stdio.h>
-
 #include "sys/log.h"
 #define LOG_MODULE "MQTT-DEMO"
 #define LOG_LEVEL LOG_LEVEL_INFO
-
 #include <string.h>
+
+/* This is the structure of broadcast messages. */
+struct broadcast_message {
+    uint8_t seqno;
+};
+
+/* This structure holds information about neighbors. */
+struct neighbor {
+    /* The ->next pointer is needed since we are placing these on a
+       Contiki list. */
+    struct neighbor *next;
+
+    /* The ->addr field holds the Rime address of the neighbor. */
+    linkaddr_t addr;
+
+    /* The ->last_rssi and ->last_lqi fields hold the Received Signal
+       Strength Indicator (RSSI) and CC2420 Link Quality Indicator (LQI)
+       values that are received for the incoming broadcast packets. */
+    uint16_t last_rssi, last_lqi;
+
+    /* Each broadcast packet contains a sequence number (seqno). The
+       ->last_seqno field holds the last sequenuce number we saw from
+       this neighbor. */
+    uint8_t last_seqno;
+
+    /* The ->avg_gap contains the average seqno gap that we have seen
+       from this neighbor. */
+    uint32_t avg_seqno_gap;
+
+};
+
+
+/* This #define defines the maximum amount of neighbors we can remember. */
+#define MAX_NEIGHBORS 16
+
+/* This MEMB() definition defines a memory pool from which we allocate
+   neighbor entries. */
+MEMB(neighbors_memb, struct neighbor, MAX_NEIGHBORS);
+
+/* The neighbors_list is a Contiki list that holds the neighbors we
+   have seen thus far. */
+LIST(neighbors_list);
+
+/* These hold the broadcast and unicast structures, respectively. */
+static struct broadcast_conn broadcast;
+
+/* These two defines are used for computing the moving average for the
+   broadcast sequence number gaps. */
+#define SEQNO_EWMA_UNITY 0x100
+#define SEQNO_EWMA_ALPHA 0x040
+
 
 /*---------------------------------------------------------------------------*/
 /*
