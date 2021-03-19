d8b79375ae6ba7780bcb38cc7a543b62f87e3dd7
User <liviobiondo@gmail.com>
2021-03-19 23:45:52 +0100

    working both broadcast and mqtt

--- examples/node - rime/mqtt-demo.c
+++ examples/node - rime/mqtt-demo.c
@@ -1,46 +1,3 @@
-/*
- * Copyright (c) 2014, Texas Instruments Incorporated - http://www.ti.com/
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-/*---------------------------------------------------------------------------*/
-/** \addtogroup cc2538-examples
- * @{
- *
- * \defgroup cc2538-mqtt-demo CC2538 MQTT Demo Project
- *
- * Demonstrates MQTT functionality. Works with IBM Quickstart as well as
- * mosquitto.
- * @{
- *
- * \file
- * An MQTT example for the cc2538-based platforms
- */
-/*---------------------------------------------------------------------------*/
 #include "contiki-conf.h"
 #include "rpl/rpl-private.h"
 #include "mqtt.h"
@@ -53,27 +10,67 @@
 #include "lib/sensors.h"
 #include "dev/button-sensor.h"
 #include "dev/leds.h"
-
-
-#include "simple-udp.h"
 #include "contiki.h"
+#include "lib/list.h"
+#include "lib/memb.h"
 #include "lib/random.h"
-#include "net/ipv6/uip-ds6.h"
-
+#include "net/rime/rime.h"
+#include <stdio.h>
 #include "sys/log.h"
 #define LOG_MODULE "MQTT-DEMO"
 #define LOG_LEVEL LOG_LEVEL_INFO
-
 #include <string.h>
-#include <stdio.h>
+
+/* This is the structure of broadcast messages. */
+struct broadcast_message {
+    uint8_t seqno;
+};
+
+/* This structure holds information about neighbors. */
+struct neighbor {
+    /* The ->next pointer is needed since we are placing these on a
+       Contiki list. */
+    struct neighbor *next;
+
+    /* The ->addr field holds the Rime address of the neighbor. */
+    linkaddr_t addr;
+
+    /* The ->last_rssi and ->last_lqi fields hold the Received Signal
+       Strength Indicator (RSSI) and CC2420 Link Quality Indicator (LQI)
+       values that are received for the incoming broadcast packets. */
+    uint16_t last_rssi, last_lqi;
+
+    /* Each broadcast packet contains a sequence number (seqno). The
+       ->last_seqno field holds the last sequenuce number we saw from
+       this neighbor. */
+    uint8_t last_seqno;
+
+    /* The ->avg_gap contains the average seqno gap that we have seen
+       from this neighbor. */
+    uint32_t avg_seqno_gap;
+
+};
 
 
-#define UDP_PORT 1234
+/* This #define defines the maximum amount of neighbors we can remember. */
+#define MAX_NEIGHBORS 16
 
-#define SEND_INTERVAL		(20 * CLOCK_SECOND)
-#define SEND_TIME		(random_rand() % (SEND_INTERVAL))
+/* This MEMB() definition defines a memory pool from which we allocate
+   neighbor entries. */
+MEMB(neighbors_memb, struct neighbor, MAX_NEIGHBORS);
 
-static struct simple_udp_connection broadcast_connection;
+/* The neighbors_list is a Contiki list that holds the neighbors we
+   have seen thus far. */
+LIST(neighbors_list);
+
+/* These hold the broadcast and unicast structures, respectively. */
+static struct broadcast_conn broadcast;
+
+/* These two defines are used for computing the moving average for the
+   broadcast sequence number gaps. */
+#define SEQNO_EWMA_UNITY 0x100
+#define SEQNO_EWMA_ALPHA 0x040
+
 
 /*---------------------------------------------------------------------------*/
 /*
@@ -139,9 +136,15 @@ static uint8_t state;
 #define DEFAULT_PUBLISH_INTERVAL    (60 * CLOCK_SECOND)
 #define DEFAULT_KEEP_ALIVE_TIMER    60
 /*---------------------------------------------------------------------------*/
+
+
+//TODO: vedere se si può cambiare process name qui sotto
+
+
 PROCESS_NAME(mqtt_demo_process);
-PROCESS(broadcast_example_process, "UDP broadcast example process");
-AUTOSTART_PROCESSES(&mqtt_demo_process,&broadcast_example_process);
+/* We first declare our two processes. */
+PROCESS(broadcast_process, "Broadcast process");
+AUTOSTART_PROCESSES(&mqtt_demo_process,&broadcast_process);
 /*---------------------------------------------------------------------------*/
 /**
  * \brief Data structure declaration for the MQTT client configuration
@@ -590,6 +593,108 @@ state_machine(void)
   etimer_set(&publish_periodic_timer, STATE_MACHINE_PERIODIC);
 }
 /*---------------------------------------------------------------------------*/
+/* This function is called whenever a broadcast message is received. */
+static void
+broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
+{
+    struct neighbor *n;
+    struct broadcast_message *m;
+    uint8_t seqno_gap;
+
+    /* The packetbuf_dataptr() returns a pointer to the first data byte
+       in the received packet. */
+    m = packetbuf_dataptr();
+
+    /* Check if we already know this neighbor. */
+    for(n = list_head(neighbors_list); n != NULL; n = list_item_next(n)) {
+
+        /* We break out of the loop if the address of the neighbor matches
+           the address of the neighbor from which we received this
+           broadcast message. */
+        if(linkaddr_cmp(&n->addr, from)) {
+            break;
+        }
+    }
+
+    /* If n is NULL, this neighbor was not found in our list, and we
+       allocate a new struct neighbor from the neighbors_memb memory
+       pool. */
+    if(n == NULL) {
+        n = memb_alloc(&neighbors_memb);
+
+        /* If we could not allocate a new neighbor entry, we give up. We
+           could have reused an old neighbor entry, but we do not do this
+           for now. */
+        if(n == NULL) {
+            return;
+        }
+
+        /* Initialize the fields. */
+        linkaddr_copy(&n->addr, from);
+        n->last_seqno = m->seqno - 1;
+        n->avg_seqno_gap = SEQNO_EWMA_UNITY;
+
+        /* Place the neighbor on the neighbor list. */
+        list_add(neighbors_list, n);
+    }
+
+    /* We can now fill in the fields in our neighbor entry. */
+    n->last_rssi = packetbuf_attr(PACKETBUF_ATTR_RSSI);
+    n->last_lqi = packetbuf_attr(PACKETBUF_ATTR_LINK_QUALITY);
+
+    /* Compute the average sequence number gap we have seen from this neighbor. */
+    seqno_gap = m->seqno - n->last_seqno;
+    n->avg_seqno_gap = (((uint32_t)seqno_gap * SEQNO_EWMA_UNITY) *
+                        SEQNO_EWMA_ALPHA) / SEQNO_EWMA_UNITY +
+                       ((uint32_t)n->avg_seqno_gap * (SEQNO_EWMA_UNITY -
+                                                      SEQNO_EWMA_ALPHA)) /
+                       SEQNO_EWMA_UNITY;
+
+    /* Remember last seqno we heard. */
+    n->last_seqno = m->seqno;
+
+    /* Print out a message. */
+    printf("broadcast message received from %d.%d with seqno %d, RSSI %u, LQI %u, avg seqno gap %d.%02d\n",
+           from->u8[0], from->u8[1],
+           m->seqno,
+           packetbuf_attr(PACKETBUF_ATTR_RSSI),
+           packetbuf_attr(PACKETBUF_ATTR_LINK_QUALITY),
+           (int)(n->avg_seqno_gap / SEQNO_EWMA_UNITY),
+           (int)(((100UL * n->avg_seqno_gap) / SEQNO_EWMA_UNITY) % 100));
+}
+/* This is where we define what function to be called when a broadcast
+   is received. We pass a pointer to this structure in the
+   broadcast_open() call below. */
+static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(broadcast_process, ev, data)
+{
+static struct etimer et;
+static uint8_t seqno;
+struct broadcast_message msg;
+
+PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+
+PROCESS_BEGIN();
+
+broadcast_open(&broadcast, 129, &broadcast_call);
+
+while(1) {
+
+/* Send a broadcast every 16 - 32 seconds */
+etimer_set(&et, CLOCK_SECOND * 16 + random_rand() % (CLOCK_SECOND * 16));
+
+PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+msg.seqno = seqno;
+packetbuf_copyfrom(&msg, sizeof(struct broadcast_message));
+broadcast_send(&broadcast);
+seqno++;
+}
+
+PROCESS_END();
+}
+
 PROCESS_THREAD(mqtt_demo_process, ev, data)
 {
 
@@ -609,46 +714,6 @@ PROCESS_THREAD(mqtt_demo_process, ev, data)
       state_machine();
     }
 
-  }
-
-  PROCESS_END();
-}
-/*---------------------------------------------------------------------------*/
-static void
-receiver(struct simple_udp_connection *c,
-         const uip_ipaddr_t *sender_addr,
-         uint16_t sender_port,
-         const uip_ipaddr_t *receiver_addr,
-         uint16_t receiver_port,
-         const uint8_t *data,
-         uint16_t datalen)
-{
-  printf("Data received on port %d from port %d with length %d\n",
-         receiver_port, sender_port, datalen);
-}
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(broadcast_example_process, ev, data)
-{
-  static struct etimer periodic_timer;
-  static struct etimer send_timer;
-  uip_ipaddr_t addr;
-
-  PROCESS_BEGIN();
-
-  simple_udp_register(&broadcast_connection, UDP_PORT,
-                      NULL, UDP_PORT,
-                      receiver);
-
-  etimer_set(&periodic_timer, SEND_INTERVAL);
-  while(1) {
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
-    etimer_reset(&periodic_timer);
-    etimer_set(&send_timer, SEND_TIME);
-
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&send_timer));
-    printf("Sending broadcast\n");
-    uip_create_linklocal_allnodes_mcast(&addr);
-    simple_udp_sendto(&broadcast_connection, "Test", 4, &addr);
   }
 
   PROCESS_END();
